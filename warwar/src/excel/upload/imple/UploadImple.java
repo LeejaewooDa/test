7d2c332a0417511b3c7745957a1eaaf04a9edd6c
KINGLE-PC\EomPC <teran1024@naver.com>
2017-07-21 22:37:30 +0900

    testPush

---
+++ warwar/src/excel/upload/imple/UploadImple.java
@@ -0,0 +1,25 @@
+package excel.upload.imple;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.ibatis.SqlMapClientTemplate;
+import org.springframework.stereotype.Repository;
+
+import excel.upload.dao.UploadDao;
+import excel.upload.domain.Excel;
+
+@Repository
+public class UploadImple implements UploadDao {
+
+	private String NAMESPACE="Excel.";
+	
+	@Autowired
+	private SqlMapClientTemplate sqlMapClientTemplate;
+
+	@Override
+	public void addExcel(List<Excel> list) {
+		System.out.println("addExcel");
+		sqlMapClientTemplate.insert(NAMESPACE+"addExcel", list);
+	}
+}
---
+++ warwar/src/excel/upload/service/UploadService.java
@@ -0,0 +1,24 @@
+package excel.upload.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import excel.upload.dao.UploadDao;
+import excel.upload.domain.Excel;
+
+@Service
+public class UploadService implements IUploadService {
+    
+	@Autowired
+	private UploadDao uploadDao;
+
+	@Override
+	public void addExcel(List<Excel> list) {
+		System.out.println("addExcel_UploadService");
+		uploadDao.addExcel(list);
+		
+	}
+
+}
